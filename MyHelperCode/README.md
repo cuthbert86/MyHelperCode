28a3b0e0698afd02d661de40638f971827dd4d9f
cuthbert86 <cuthbert.corp@gmail.com>
2026-04-11 10:11:17 +0100

    Initial commit

---
+++ MyHelperCode/README.md
@@ -0,0 +1,10 @@
+# MyHelperCode
+
+A collection of incredibly useful functions and methods for MicroPython programs running on Raspberry Pi Pico.
+
+## Installation
+
+```bash
+pip install myhelpercodefrom myhelpercode import some_function
+
+result = some_function()
---
+++ MyHelperCode/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="myhelpercode",                    # PyPI package name (lowercase)
+    version="0.1.0",                        # Semantic versioning (major.minor.patch)
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="Useful helper functions for MicroPython on Pico",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/cuthbert86/MyHelperCode",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: MicroPython",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    keywords="micropython pico helpers utilities",
+)
